956dec52caa84db69773bb89c215291ad9b512c8
RakeshGowda <rakeshgowda3101@gmail.com>
2020-01-14 14:50:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Usurvey from './usurvey';
+
+function App() {
+  return (
+    <div className="App">
+      <Usurvey />
+    </div>
+  );
+}
+
+export default App;
